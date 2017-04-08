53b2712d25a65b55b42a05bfa1d4bf3f869613e4
Brett Schules <brettschules@gmail.com>
2017-04-08 21:00:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+   data.each do |demo, demo_value|
+     demo_value.each do |list,names|
+         names.each do |name|
+         pigeon_list[name] ||= {}
+         pigeon_list[name][demo] ||= []
+         pigeon_list[name][demo] << list.to_s
+       end
+     end
+   end
+ pigeon_list
 end
